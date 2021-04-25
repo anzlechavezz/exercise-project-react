9ddfd1ce3b19c010c6a1ac517f6c4ee23e516f31
anzlechavezz <anzleweirdo@gmail.com>
2021-04-25 17:20:19 -0400

    💪🏽 finished create-exercise and create-user components

--- src/components/navbar.component.jsx
+++ src/components/navbar.component.jsx
@@ -6,7 +6,7 @@ export default class Navbar extends Component {
         return (
             <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
                 <Link to="/" className="navbar-brand">ExcerTracker</Link>
-                <div className="collapse navbar-collapse">ExcerTracker</div>
+                <div className="collapse navbar-collapse"></div>
                 <ul className="navbar-nav mr-auto">
                     <li className="navbar-item">
                     <Link to="/" className="nav-link">Exercises</Link>
